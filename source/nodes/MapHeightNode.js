6ad87fa55ef3d9999059321cb3ba512caa19661a
tentone <tentone@outlook.com>
2021-04-28 12:01:14 +0100

    Moving code to es6

--- source/nodes/MapHeightNode.js
+++ source/nodes/MapHeightNode.js
@@ -19,220 +19,216 @@ import {MapNode} from "./MapNode.js";
  * @param material {Material} Material used to render this height node.
  * @param geometry {Geometry} Geometry used to render this height node.
  */
-function MapHeightNode(parentNode, mapView, location, level, x, y, material, geometry)
-{
-	if (material === undefined)
+export class MapHeightNode extends Mesh{
+	constructor(parentNode, mapView, location, level, x, y, material, geometry)
 	{
-		material = new MeshPhongMaterial(
-			{
-				color: 0x000000,
-				specular: 0x000000,
-				shininess: 0,
-				wireframe: false,
-				emissive: 0xFFFFFF
-			});
-	}
-
-	Mesh.call(this, geometry === undefined ? MapHeightNode.GEOMETRY: geometry, material);
-	MapNode.call(this, parentNode, mapView, location, level, x, y);
-
-	this.matrixAutoUpdate = false;
-	this.isMesh = true;
-	
-	this.visible = false;
-
-	/**
-	 * Flag indicating if the tile texture was loaded.
-	 * 
-	 * @attribute textureLoaded
-	 * @type {boolean}
-	 */
-	this.textureLoaded = false;
-
-	/**
-	 * Flag indicating if the tile height data was loaded.
-	 * 
-	 * @attribute heightLoaded
-	 * @type {boolean}
-	 */
-	this.heightLoaded = false;
-
-	this.loadTexture();
-}
-
-MapHeightNode.prototype = Object.create(Mesh.prototype);
-Object.assign(MapHeightNode.prototype, MapNode.prototype);
-
-MapHeightNode.prototype.constructor = MapHeightNode;
-
-/**
- * Original tile size of the images retrieved from the height provider.
- *
- * @static
- * @attribute TILE_SIZE
- * @type {number}
- */
-MapHeightNode.TILE_SIZE = 256;
-
-/**
- * Size of the grid of the geometry displayed on the scene for each tile.
- *
- * @static
- * @attribute GEOMETRY_SIZE
- * @type {number}
- */
-MapHeightNode.GEOMETRY_SIZE = 16;
-
-/**
- * Map node plane geometry.
- *
- * @static
- * @attribute GEOMETRY
- * @type {PlaneBufferGeometry}
- */
-MapHeightNode.GEOMETRY = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
-
-/**
- * Load tile texture from the server.
- * 
- * Aditionally in this height node it loads elevation data from the height provider and generate the appropiate maps.
- *
- * @method loadTexture
- */
-MapHeightNode.prototype.loadTexture = function()
-{
-	var self = this;
-
-	this.mapView.fetchTile(this.level, this.x, this.y).then(function(image)
-	{
-		var texture = new Texture(image);
-		texture.generateMipmaps = false;
-		texture.format = RGBFormat;
-		texture.magFilter = LinearFilter;
-		texture.minFilter = LinearFilter;
-		texture.needsUpdate = true;
-		
-		self.material.emissiveMap = texture;
-
-		self.textureLoaded = true;
-		self.nodeReady();
-	});
-
-	this.loadHeightGeometry();
-};
-
-MapHeightNode.prototype.nodeReady = function()
-{
-	if (!this.heightLoaded || !this.textureLoaded)
-	{
-		return;
-	}
-
-	this.visible = true;
-
-	MapNode.prototype.nodeReady.call(this);
-};
-
-MapHeightNode.prototype.createChildNodes = function()
-{
-	var level = this.level + 1;
-
-	var x = this.x * 2;
-	var y = this.y * 2;
-
-	var node = new this.constructor(this, this.mapView, MapNode.TOP_LEFT, level, x, y);
-	node.scale.set(0.5, 1, 0.5);
-	node.position.set(-0.25, 0, -0.25);
-	this.add(node);
-	node.updateMatrix();
-	node.updateMatrixWorld(true);
-
-	var node = new this.constructor(this, this.mapView, MapNode.TOP_RIGHT, level, x + 1, y);
-	node.scale.set(0.5, 1, 0.5);
-	node.position.set(0.25, 0, -0.25);
-	this.add(node);
-	node.updateMatrix();
-	node.updateMatrixWorld(true);
-
-	var node = new this.constructor(this, this.mapView, MapNode.BOTTOM_LEFT, level, x, y + 1);
-	node.scale.set(0.5, 1, 0.5);
-	node.position.set(-0.25, 0, 0.25);
-	this.add(node);
-	node.updateMatrix();
-	node.updateMatrixWorld(true);
-
-	var node = new this.constructor(this, this.mapView, MapNode.BOTTOM_RIGHT, level, x + 1, y + 1);
-	node.scale.set(0.5, 1, 0.5);
-	node.position.set(0.25, 0, 0.25);
-	this.add(node);
-	node.updateMatrix();
-	node.updateMatrixWorld(true);
-};
-
-/** 
- * Load height texture from the server and create a geometry to match it.
- *
- * @method loadHeightGeometry
- * @return {Promise<void>} Returns a promise indicating when the geometry generation has finished. 
- */
-MapHeightNode.prototype.loadHeightGeometry = function()
-{
-	if (this.mapView.heightProvider === null)
-	{
-		throw new Error("GeoThree: MapView.heightProvider provider is null.");
-	}
-	
-	var self = this;
-
-	this.mapView.heightProvider.fetchTile(this.level, this.x, this.y).then(function(image)
-	{
-		var geometry = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
-		var vertices = geometry.attributes.position.array;
-	
-		var canvas = new OffscreenCanvas(MapHeightNode.GEOMETRY_SIZE + 1, MapHeightNode.GEOMETRY_SIZE + 1);
-
-		var context = canvas.getContext("2d");
-		context.imageSmoothingEnabled = false;
-		context.drawImage(image, 0, 0, MapHeightNode.TILE_SIZE, MapHeightNode.TILE_SIZE, 0, 0, canvas.width, canvas.height);
-		
-		var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-		var data = imageData.data;
-		for (var i = 0, j = 0; i < data.length && j < vertices.length; i += 4, j += 3)
+		if (material === undefined)
 		{
-			var r = data[i];
-			var g = data[i + 1];
-			var b = data[i + 2];
-
-			// The value will be composed of the bits RGB
-			var value = (r * 65536 + g * 256 + b) * 0.1 - 1e4;
-
-			vertices[j + 1] = value;
+			material = new MeshPhongMaterial(
+				{
+					color: 0x000000,
+					specular: 0x000000,
+					shininess: 0,
+					wireframe: false,
+					emissive: 0xFFFFFF
+				});
 		}
-
-		self.geometry = geometry;
-		self.heightLoaded = true;
-		self.nodeReady();
-	}).catch(function()
-	{
-		console.error("GeoThree: Failed to load height node data.", this);
-		self.heightLoaded = true;
-		self.nodeReady();
-	});
-};
-
-/**
- * Overrides normal raycasting, to avoid raycasting when isMesh is set to false.
- * 
- * @method raycast
- */
-MapHeightNode.prototype.raycast = function(raycaster, intersects)
-{
-	if (this.isMesh === true)
-	{
-		return Mesh.prototype.raycast.call(this, raycaster, intersects);
+	
+		super(geometry === undefined ? MapHeightNode.GEOMETRY: geometry, material);
+		
+		// MapNode.call(this, parentNode, mapView, location, level, x, y);
+	
+		this.matrixAutoUpdate = false;
+		this.isMesh = true;
+		
+		this.visible = false;
+	
+		/**
+		 * Flag indicating if the tile texture was loaded.
+		 * 
+		 * @attribute textureLoaded
+		 * @type {boolean}
+		 */
+		this.textureLoaded = false;
+	
+		/**
+		 * Flag indicating if the tile height data was loaded.
+		 * 
+		 * @attribute heightLoaded
+		 * @type {boolean}
+		 */
+		this.heightLoaded = false;
+	
+		this.loadTexture();
 	}
-
-	return false;
-};
-
-export {MapHeightNode};
+	
+	/**
+	 * Original tile size of the images retrieved from the height provider.
+	 *
+	 * @static
+	 * @attribute TILE_SIZE
+	 * @type {number}
+	 */
+	static TILE_SIZE = 256;
+	
+	/**
+	 * Size of the grid of the geometry displayed on the scene for each tile.
+	 *
+	 * @static
+	 * @attribute GEOMETRY_SIZE
+	 * @type {number}
+	 */
+	 static GEOMETRY_SIZE = 16;
+	
+	/**
+	 * Map node plane geometry.
+	 *
+	 * @static
+	 * @attribute GEOMETRY
+	 * @type {PlaneBufferGeometry}
+	 */
+	 static GEOMETRY = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
+	
+	/**
+	 * Load tile texture from the server.
+	 * 
+	 * Aditionally in this height node it loads elevation data from the height provider and generate the appropiate maps.
+	 *
+	 * @method loadTexture
+	 */
+	loadTexture()
+	{
+		var self = this;
+	
+		this.mapView.fetchTile(this.level, this.x, this.y).then(function(image)
+		{
+			var texture = new Texture(image);
+			texture.generateMipmaps = false;
+			texture.format = RGBFormat;
+			texture.magFilter = LinearFilter;
+			texture.minFilter = LinearFilter;
+			texture.needsUpdate = true;
+			
+			self.material.emissiveMap = texture;
+	
+			self.textureLoaded = true;
+			self.nodeReady();
+		});
+	
+		this.loadHeightGeometry();
+	};
+	
+	nodeReady()
+	{
+		if (!this.heightLoaded || !this.textureLoaded)
+		{
+			return;
+		}
+	
+		this.visible = true;
+	
+		MapNode.prototype.nodeReady.call(this);
+	};
+	
+	createChildNodes()
+	{
+		var level = this.level + 1;
+	
+		var x = this.x * 2;
+		var y = this.y * 2;
+	
+		var node = new this.constructor(this, this.mapView, MapNode.TOP_LEFT, level, x, y);
+		node.scale.set(0.5, 1, 0.5);
+		node.position.set(-0.25, 0, -0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+	
+		var node = new this.constructor(this, this.mapView, MapNode.TOP_RIGHT, level, x + 1, y);
+		node.scale.set(0.5, 1, 0.5);
+		node.position.set(0.25, 0, -0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+	
+		var node = new this.constructor(this, this.mapView, MapNode.BOTTOM_LEFT, level, x, y + 1);
+		node.scale.set(0.5, 1, 0.5);
+		node.position.set(-0.25, 0, 0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+	
+		var node = new this.constructor(this, this.mapView, MapNode.BOTTOM_RIGHT, level, x + 1, y + 1);
+		node.scale.set(0.5, 1, 0.5);
+		node.position.set(0.25, 0, 0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+	};
+	
+	/** 
+	 * Load height texture from the server and create a geometry to match it.
+	 *
+	 * @method loadHeightGeometry
+	 * @return {Promise<void>} Returns a promise indicating when the geometry generation has finished. 
+	 */
+	loadHeightGeometry()
+	{
+		if (this.mapView.heightProvider === null)
+		{
+			throw new Error("GeoThree: MapView.heightProvider provider is null.");
+		}
+		
+		var self = this;
+	
+		this.mapView.heightProvider.fetchTile(this.level, this.x, this.y).then(function(image)
+		{
+			var geometry = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
+			var vertices = geometry.attributes.position.array;
+		
+			var canvas = new OffscreenCanvas(MapHeightNode.GEOMETRY_SIZE + 1, MapHeightNode.GEOMETRY_SIZE + 1);
+	
+			var context = canvas.getContext("2d");
+			context.imageSmoothingEnabled = false;
+			context.drawImage(image, 0, 0, MapHeightNode.TILE_SIZE, MapHeightNode.TILE_SIZE, 0, 0, canvas.width, canvas.height);
+			
+			var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+			var data = imageData.data;
+			for (var i = 0, j = 0; i < data.length && j < vertices.length; i += 4, j += 3)
+			{
+				var r = data[i];
+				var g = data[i + 1];
+				var b = data[i + 2];
+	
+				// The value will be composed of the bits RGB
+				var value = (r * 65536 + g * 256 + b) * 0.1 - 1e4;
+	
+				vertices[j + 1] = value;
+			}
+	
+			self.geometry = geometry;
+			self.heightLoaded = true;
+			self.nodeReady();
+		}).catch(function()
+		{
+			console.error("GeoThree: Failed to load height node data.", this);
+			self.heightLoaded = true;
+			self.nodeReady();
+		});
+	};
+	
+	/**
+	 * Overrides normal raycasting, to avoid raycasting when isMesh is set to false.
+	 * 
+	 * @method raycast
+	 */
+	raycast(raycaster, intersects)
+	{
+		if (this.isMesh === true)
+		{
+			return Mesh.prototype.raycast.call(this, raycaster, intersects);
+		}
+	
+		return false;
+	};
+}
--- source/nodes/MapHeightNodeShader.js
+++ source/nodes/MapHeightNodeShader.js
@@ -1,177 +1,175 @@
-import {Texture, LinearFilter, RGBFormat, MeshBasicMaterial, MeshPhongMaterial, Mesh, NearestFilter} from "three";
+import {Texture, LinearFilter, RGBFormat, MeshPhongMaterial, Mesh, NearestFilter} from "three";
 import {MapHeightNode} from "./MapHeightNode.js";
 import {MapNodeGeometry} from "../geometries/MapNodeGeometry.js";
 import {MapPlaneNode} from "./MapPlaneNode.js";
-/** 
- * Map height node that uses GPU height calculation to generate the deformed plane mesh.
- * 
- * This solution is faster if no mesh interaction is required since all trasnformations are done in the GPU the transformed mesh cannot be accessed for CPU operations (e.g. raycasting).
- *
- * @class MapHeightNodeShader
- * @param parentNode {MapHeightNode} The parent node of this node.
- * @param mapView {MapView} Map view object where this node is placed.
- * @param location {number} Position in the node tree relative to the parent.
- * @param level {number} Zoom level in the tile tree of the node.
- * @param x {number} X position of the node in the tile tree.
- * @param y {number} Y position of the node in the tile tree.
- */
-function MapHeightNodeShader(parentNode, mapView, location, level, x, y)
-{
-	var material = new MeshPhongMaterial({map: MapHeightNodeShader.EMPTY_TEXTURE});
-	material = MapHeightNodeShader.prepareMaterial(material);
 
-	MapHeightNode.call(this, parentNode, mapView, location, level, x, y, material, MapHeightNodeShader.GEOMETRY);
-
-	this.frustumCulled = false;
-}
-
-MapHeightNodeShader.prototype = Object.create(MapHeightNode.prototype);
-MapHeightNodeShader.prototype.constructor = MapHeightNodeShader;
-
-/**
- * Empty texture used as a placeholder for missing textures.
- * 
- * @static
- * @attribute EMPTY_TEXTURE
- * @type {Texture}
- */
-MapHeightNodeShader.EMPTY_TEXTURE = new Texture();
-
-/**
- * Size of the grid of the geometry displayed on the scene for each tile.
- * 
- * @static
- * @attribute GEOMETRY_SIZE
- * @type {number}
- */
-MapHeightNodeShader.GEOMETRY_SIZE = 256;
-
-/**
- * Map node plane geometry.
- *
- * @static
- * @attribute GEOMETRY
- * @type {PlaneBufferGeometry}
- */
-MapHeightNodeShader.GEOMETRY = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
-
-/**
- * Prepare the threejs material to be used in the map tile.
- * 
- * @param {Material} material Material to be transformed. 
- */
-MapHeightNodeShader.prepareMaterial = function(material)
-{
-	material.userData = {heightMap: {value: MapHeightNodeShader.EMPTY_TEXTURE}};
-
-	material.onBeforeCompile = (shader) =>
+export class MapHeightNode extends MapHeightNode {
+	/** 
+	 * Map height node that uses GPU height calculation to generate the deformed plane mesh.
+	 * 
+	 * This solution is faster if no mesh interaction is required since all trasnformations are done in the GPU the transformed mesh cannot be accessed for CPU operations (e.g. raycasting).
+	 *
+	 * @class MapHeightNodeShader
+	 * @param parentNode {MapHeightNode} The parent node of this node.
+	 * @param mapView {MapView} Map view object where this node is placed.
+	 * @param location {number} Position in the node tree relative to the parent.
+	 * @param level {number} Zoom level in the tile tree of the node.
+	 * @param x {number} X position of the node in the tile tree.
+	 * @param y {number} Y position of the node in the tile tree.
+	 */
+	constructor(parentNode, mapView, location, level, x, y)
 	{
-		// Pass uniforms from userData to the
-		for (let i in material.userData)
-		{
-			shader.uniforms[i] = material.userData[i];
-		}
-
-		// Vertex variables
-		shader.vertexShader = `
-		uniform sampler2D heightMap;
-		` + shader.vertexShader;
-
-		// Vertex depth logic
-		shader.vertexShader = shader.vertexShader.replace("#include <fog_vertex>", `
-		#include <fog_vertex>
-
-		// Calculate height of the title
-		vec4 _theight = texture2D(heightMap, vUv);
-		float _height = ((_theight.r * 255.0 * 65536.0 + _theight.g * 255.0 * 256.0 + _theight.b * 255.0) * 0.1) - 10000.0;
-		vec3 _transformed = position + _height * normal;
-
-		// Vertex position based on height
-		gl_Position = projectionMatrix * modelViewMatrix * vec4(_transformed, 1.0);
-		`);
-	};
-
-	return material;
-};
-
-MapHeightNodeShader.prototype.loadTexture = function()
-{
-	var self = this;
-
-	this.mapView.fetchTile(this.level, this.x, this.y).then(function(image)
-	{
-		var texture = new Texture(image);
-		texture.generateMipmaps = false;
-		texture.format = RGBFormat;
-		texture.magFilter = LinearFilter;
-		texture.minFilter = LinearFilter;
-		texture.needsUpdate = true;
-
-		self.material.map = texture;
-
-		self.textureLoaded = true;
-		self.nodeReady();
-	}).catch(function(err)
-	{
-		console.error("GeoThree: Failed to load color node data.", err);
-		self.textureLoaded = true;
-		self.nodeReady();
-	});
-
-	this.loadHeightGeometry();
-};
-
-MapHeightNodeShader.prototype.loadHeightGeometry = function()
-{
-	if (this.mapView.heightProvider === null)
-	{
-		throw new Error("GeoThree: MapView.heightProvider provider is null.");
+		var material = new MeshPhongMaterial({map: MapHeightNodeShader.EMPTY_TEXTURE});
+		material = MapHeightNodeShader.prepareMaterial(material);
+	
+		super(parentNode, mapView, location, level, x, y, material, MapHeightNodeShader.GEOMETRY);
+	
+		this.frustumCulled = false;
 	}
 	
-	var self = this;
-
-	this.mapView.heightProvider.fetchTile(this.level, this.x, this.y).then(function(image)
+	/**
+	 * Empty texture used as a placeholder for missing textures.
+	 * 
+	 * @static
+	 * @attribute EMPTY_TEXTURE
+	 * @type {Texture}
+	 */
+	static EMPTY_TEXTURE = new Texture();
+	
+	/**
+	 * Size of the grid of the geometry displayed on the scene for each tile.
+	 * 
+	 * @static
+	 * @attribute GEOMETRY_SIZE
+	 * @type {number}
+	 */
+	static GEOMETRY_SIZE = 256;
+	
+	/**
+	 * Map node plane geometry.
+	 *
+	 * @static
+	 * @attribute GEOMETRY
+	 * @type {PlaneBufferGeometry}
+	 */
+	static GEOMETRY = new MapNodeGeometry(1, 1, MapHeightNode.GEOMETRY_SIZE, MapHeightNode.GEOMETRY_SIZE);
+	
+	/**
+	 * Prepare the threejs material to be used in the map tile.
+	 * 
+	 * @param {Material} material Material to be transformed. 
+	 */
+	static prepareMaterial(material)
 	{
-		var texture = new Texture(image);
-		texture.generateMipmaps = false;
-		texture.format = RGBFormat;
-		texture.magFilter = NearestFilter;
-		texture.minFilter = NearestFilter;
-		texture.needsUpdate = true;
-
-		self.material.userData.heightMap.value = texture;
-		
-		self.heightLoaded = true;
-		self.nodeReady();
-	}).catch(function(err)
+		material.userData = {heightMap: {value: MapHeightNodeShader.EMPTY_TEXTURE}};
+	
+		material.onBeforeCompile = (shader) =>
+		{
+			// Pass uniforms from userData to the
+			for (let i in material.userData)
+			{
+				shader.uniforms[i] = material.userData[i];
+			}
+	
+			// Vertex variables
+			shader.vertexShader = `
+			uniform sampler2D heightMap;
+			` + shader.vertexShader;
+	
+			// Vertex depth logic
+			shader.vertexShader = shader.vertexShader.replace("#include <fog_vertex>", `
+			#include <fog_vertex>
+	
+			// Calculate height of the title
+			vec4 _theight = texture2D(heightMap, vUv);
+			float _height = ((_theight.r * 255.0 * 65536.0 + _theight.g * 255.0 * 256.0 + _theight.b * 255.0) * 0.1) - 10000.0;
+			vec3 _transformed = position + _height * normal;
+	
+			// Vertex position based on height
+			gl_Position = projectionMatrix * modelViewMatrix * vec4(_transformed, 1.0);
+			`);
+		};
+	
+		return material;
+	};
+	
+	loadTexture()
 	{
-		console.error("GeoThree: Failed to load height node data.", err);
-		self.heightLoaded = true;
-		self.nodeReady();
-	});
-};
-
-/**
- * Overrides normal raycasting, to avoid raycasting when isMesh is set to false.
- * 
- * Switches the geometry for a simpler one for faster raycasting.
- * 
- * @method raycast
- */
-MapHeightNodeShader.prototype.raycast = function(raycaster, intersects)
-{
-	if (this.isMesh === true)
-	{
-		this.geometry = MapPlaneNode.GEOMETRY;
-
-		var result = Mesh.prototype.raycast.call(this, raycaster, intersects);
-
-		this.geometry = MapHeightNodeShader.GEOMETRY;
-
-		return result;
+		var self = this;
+	
+		this.mapView.fetchTile(this.level, this.x, this.y).then(function(image)
+		{
+			var texture = new Texture(image);
+			texture.generateMipmaps = false;
+			texture.format = RGBFormat;
+			texture.magFilter = LinearFilter;
+			texture.minFilter = LinearFilter;
+			texture.needsUpdate = true;
+	
+			self.material.map = texture;
+	
+			self.textureLoaded = true;
+			self.nodeReady();
+		}).catch(function(err)
+		{
+			console.error("GeoThree: Failed to load color node data.", err);
+			self.textureLoaded = true;
+			self.nodeReady();
+		});
+	
+		this.loadHeightGeometry();
 	}
-
-	return false;
-};
-
-export {MapHeightNodeShader};
+	
+	loadHeightGeometry()
+	{
+		if (this.mapView.heightProvider === null)
+		{
+			throw new Error("GeoThree: MapView.heightProvider provider is null.");
+		}
+		
+		var self = this;
+	
+		this.mapView.heightProvider.fetchTile(this.level, this.x, this.y).then(function(image)
+		{
+			var texture = new Texture(image);
+			texture.generateMipmaps = false;
+			texture.format = RGBFormat;
+			texture.magFilter = NearestFilter;
+			texture.minFilter = NearestFilter;
+			texture.needsUpdate = true;
+	
+			self.material.userData.heightMap.value = texture;
+			
+			self.heightLoaded = true;
+			self.nodeReady();
+		}).catch(function(err)
+		{
+			console.error("GeoThree: Failed to load height node data.", err);
+			self.heightLoaded = true;
+			self.nodeReady();
+		});
+	}
+	
+	/**
+	 * Overrides normal raycasting, to avoid raycasting when isMesh is set to false.
+	 * 
+	 * Switches the geometry for a simpler one for faster raycasting.
+	 * 
+	 * @method raycast
+	 */
+	raycast(raycaster, intersects)
+	{
+		if (this.isMesh === true)
+		{
+			this.geometry = MapPlaneNode.GEOMETRY;
+	
+			var result = Mesh.prototype.raycast.call(this, raycaster, intersects);
+	
+			this.geometry = MapHeightNodeShader.GEOMETRY;
+	
+			return result;
+		}
+	
+		return false;
+	}
+}
