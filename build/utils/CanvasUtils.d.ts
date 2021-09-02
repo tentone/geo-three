4fd3a5ca071d070b03b5b17f1ce0d03b08c0c094
tentone <tentone@outlook.com>
2021-09-02 19:07:46 +0100

    Added offscreen canvas types

---
+++ build/utils/CanvasUtils.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="offscreencanvas" />
+export declare class CanvasUtils {
+    static createOffscreenCanvas(width: number, height: number): (HTMLCanvasElement | OffscreenCanvas);
+}
