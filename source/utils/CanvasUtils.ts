3ada24e7befadcf25d20ae462fa3aebf2e97fb1a
tentone <tentone@outlook.com>
2022-02-23 23:15:22 +0000

    Update dependencies and lint code

--- source/utils/CanvasUtils.ts
+++ source/utils/CanvasUtils.ts
@@ -1,8 +1,9 @@
 /**
  * Contains utils to handle canvas element manipulation and common canvas operations.
  */
-export class CanvasUtils {
-    /**
+export class CanvasUtils 
+{
+	/**
      * Create a offscreen canvas, used to draw content that will not be displayed using DOM.
      * 
      * If OffscreenCanvas object is no available creates a regular DOM canvas object instead.
@@ -10,14 +11,18 @@ export class CanvasUtils {
      * @param width - Width of the canvas in pixels.
      * @param height - Height of the canvas in pixels.
      */
-    public static createOffscreenCanvas(width: number, height: number): (HTMLCanvasElement | OffscreenCanvas) {
-        if (OffscreenCanvas) {
-            return new OffscreenCanvas(width, height)
-        } else {
-            let canvas = document.createElement('canvas');
-            canvas.width = width;
-            canvas.height = height;
-            return canvas;
-        }
-    }
+	public static createOffscreenCanvas(width: number, height: number): (HTMLCanvasElement | OffscreenCanvas) 
+	{
+		if (OffscreenCanvas) 
+		{
+			return new OffscreenCanvas(width, height);
+		}
+		else 
+		{
+			let canvas = document.createElement('canvas');
+			canvas.width = width;
+			canvas.height = height;
+			return canvas;
+		}
+	}
 }
