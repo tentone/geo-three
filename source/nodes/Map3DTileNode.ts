8e0b4ca72bd1f21975195050a981206d02cfe5a3
Jose Ferrao <tentone@outlook.com>
2023-05-31 16:13:30 +0100

    Derp

---
+++ source/nodes/Map3DTileNode.ts
@@ -0,0 +1,83 @@
+import {BoxGeometry, BufferGeometry, Intersection, MeshBasicMaterial, Raycaster, Vector3} from 'three';
+import {MapNode, QuadTreePosition} from './MapNode';
+import {MapNodeGeometry} from '../geometries/MapNodeGeometry';
+import {UnitsUtils} from '../utils/UnitsUtils';
+import { MapPlaneNode } from './MapPlaneNode';
+
+/**
+ * Represents a tile node for a 3D tiles format.
+ * 
+ * Details about 3D Tiles format at https://www.ogc.org/standard/3dtiles/.
+ */
+export class Map3DTileNode extends MapNode 
+{
+	public constructor(parentNode = null, mapView = null, location = QuadTreePosition.root, level = 0, x = 0, y = 0) 
+	{
+		super(parentNode, mapView, location, level, x, y, new BoxGeometry(), new MeshBasicMaterial());
+
+		this.matrixAutoUpdate = false;
+		this.isMesh = true;
+		this.visible = false;
+	}
+
+	public static baseGeometry: BufferGeometry = new MapNodeGeometry(1, 1, 1, 1, false);
+
+	public static baseScale: Vector3 = new Vector3(UnitsUtils.EARTH_PERIMETER, 1.0, UnitsUtils.EARTH_PERIMETER);
+
+	public async initialize(): Promise<void>
+	{
+		super.initialize();
+		
+		await this.loadData();
+		
+		this.nodeReady();
+	}
+
+	public createChildNodes(): void
+	{
+		const level = this.level + 1;
+		const x = this.x * 2;
+		const y = this.y * 2;
+
+		const Constructor = Object.getPrototypeOf(this).constructor;
+		
+		let node = new Constructor(this, this.mapView, QuadTreePosition.topLeft, level, x, y);
+		node.scale.set(0.5, 1.0, 0.5);
+		node.position.set(-0.25, 0, -0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+
+		node = new Constructor(this, this.mapView, QuadTreePosition.topRight, level, x + 1, y);
+		node.scale.set(0.5, 1.0, 0.5);
+		node.position.set(0.25, 0, -0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+
+		node = new Constructor(this, this.mapView, QuadTreePosition.bottomLeft, level, x, y + 1);
+		node.scale.set(0.5, 1.0, 0.5);
+		node.position.set(-0.25, 0, 0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+
+		node = new Constructor(this, this.mapView, QuadTreePosition.bottomRight, level, x + 1, y + 1);
+		node.scale.set(0.5, 1.0, 0.5);
+		node.position.set(0.25, 0, 0.25);
+		this.add(node);
+		node.updateMatrix();
+		node.updateMatrixWorld(true);
+	}
+
+	/**
+	 * Overrides normal raycasting, to avoid raycasting when isMesh is set to false.
+	 */
+	public raycast(raycaster: Raycaster, intersects: Intersection[]): void
+	{
+		if (this.isMesh === true) 
+		{
+			super.raycast(raycaster, intersects);
+		}
+	}
+}
